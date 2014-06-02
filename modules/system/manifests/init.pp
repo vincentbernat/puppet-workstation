544a46b48d68c25c28dd0e802b2f3851da84ec0c
Vincent Bernat <vincent@bernat.im>
2014-06-02 10:20:05 +0200

    system: disable defragmentation of transparent huge pages
    The system can come to an halt when copying to slow USB devices because
    of this.

--- modules/system/manifests/init.pp
+++ modules/system/manifests/init.pp
@@ -7,4 +7,13 @@ class system {
   package { "resolvconf": ensure => installed }
   package { "ca-certificates": ensure => installed }
   package { "systemd-sysv": ensure => installed }
+
+  # Defragmentation of transparent huge page can slow down a host when
+  # copying to slow devices (like USB keys)
+  file { "/etc/sysfs.d/usb-performance.conf":
+    content => "kernel/mm/transparent_hugepage/defrag = madvise",
+    notify => Service["sysfsutils"]
+  }
+
+  service { "sysfsutils": }
 }
