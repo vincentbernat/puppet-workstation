0e95fa0103bbf51b100aff06fd76af71d5084d2e
Vincent Bernat <vincent@bernat.im>
2014-06-02 21:37:54 +0200

    disk: use external udev modules to manage udev rules

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,11 +4,13 @@ File {
 }
 
 node default {
+  # external modules
   class { "apt":
     purge_sources_list   => true,
     purge_sources_list_d => true,
     purge_preferences_d  => true
   }
+  class { "udev": }
 
   # The current user is virtual to allow modifications from multiple places
   @user { "${::user}":
--- modules/system/manifests/disk.pp
+++ modules/system/manifests/disk.pp
@@ -1,6 +1,6 @@
 class disk {
 
-  file {'/etc/udev/rules.d/60-schedulers.rules':
+  udev::rule { "60-schedulers.rules":
     source => 'puppet:///modules/system/disk/schedulers.rules',
   }
 
