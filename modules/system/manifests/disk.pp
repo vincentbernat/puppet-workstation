2b6ccbcdd0482b6c8a4d2262a56acee16124745f
Vincent Bernat <vincent@bernat.im>
2014-06-02 21:52:18 +0200

    disk: enable discard for LVM

--- modules/system/manifests/disk.pp
+++ modules/system/manifests/disk.pp
@@ -4,4 +4,13 @@ class disk {
     source => 'puppet:///modules/system/disk/schedulers.rules',
   }
 
+  # discards should be enabled when supported (on SSD)
+  package { "lvm2": ensure => installed }
+  ->
+  file_line { 'lvm should enable discard':
+    ensure => present,
+    line   => 'issue_discards = 1',
+    match  => 'issue_discards = .',
+    path   => '/etc/lvm/lvm.conf',
+  }
 }
