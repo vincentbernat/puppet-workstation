a9b0e82f9efc90058dbc4db6a10db680c75a5904
Vincent Bernat <vincent@bernat.im>
2014-06-07 19:32:21 +0200

    skype: try to fix skype installation
    Maybe one day, Microsoft will learn about APT repositories...

--- modules/desktop/manifests/skype.pp
+++ modules/desktop/manifests/skype.pp
@@ -1,9 +1,16 @@
 class skype {
 
+  exec { 'retrieve_skype':
+    command => "/usr/bin/wget -q http://www.skype.com/go/getskype-linux-deb-32 -O /opt/skype.deb",
+    creates => "/opt/skype.deb",
+  }
+
+  # This won't work as the dependencies are missing. The use has to do "apt-get install -f".
   package { "skype":
     provider => dpkg,
     ensure   => installed,
-    source   => "http://www.skype.com/go/getskype-linux-deb-32"
+    source   => "/opt/skype.deb",
+    require  => Exec['retrieve_skype']
   }
   ->
   package { "gtk2-engines-pixbuf:i386": ensure => installed }
