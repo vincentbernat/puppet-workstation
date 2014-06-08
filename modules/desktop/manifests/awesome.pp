c34ea4b8b1f5ebdfc263b657f92eb2494d935daf
Vincent Bernat <vincent@bernat.im>
2014-06-08 22:13:59 +0200

    awesome: install python-xpyb to handle wallpapers

--- modules/desktop/manifests/awesome.pp
+++ modules/desktop/manifests/awesome.pp
@@ -6,15 +6,16 @@ class awesome {
   package { "dconf-cli": ensure => installed }
   package { "fvwm": ensure => installed }
   package { "fvwm-crystal": ensure => installed }
-  package { "i3lock": ensure => installed }
   package { "gconf2": ensure => installed }
   package { "gnome-icon-theme": ensure => installed }
   package { "gnome-icon-theme-symbolic": ensure => installed }
   package { "gnome-themes-standard": ensure => installed }
   package { "gnupg-agent": ensure => installed }
   package { "hsetroot": ensure => installed }
+  package { "i3lock": ensure => installed }
   package { "numlockx": ensure => installed }
   package { "policykit-1-gnome": ensure => installed }
+  package { "python-xpyb": ensure => installed }
   package { "redshift": ensure => installed }
   package { "x11-xkb-utils": ensure => installed }
   package { "x11-xserver-utils": ensure => installed }
