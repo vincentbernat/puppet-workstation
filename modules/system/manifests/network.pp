27e181eb4cc8320b855ecc2728f999a370d95980
Vincent Bernat <vincent@bernat.im>
2014-06-08 22:24:14 +0200

    network: more network-manager packages

--- modules/system/manifests/network.pp
+++ modules/system/manifests/network.pp
@@ -6,6 +6,12 @@ class network {
   package { "network-manager-gnome": ensure => installed }
   package { "network-manager-openvpn": ensure => installed }
   package { "network-manager-openvpn-gnome": ensure => installed }
+  package { "network-manager-openconnect": ensure => installed }
+  package { "network-manager-openconnect-gnome": ensure => installed }
+  package { "network-manager-pptp": ensure => installed }
+  package { "network-manager-pptp-gnome": ensure => installed }
+  package { "network-manager-vpnc": ensure => installed }
+  package { "network-manager-vpnc-gnome": ensure => installed }
 
   file { "/etc/network/interfaces":
     source => "puppet:///modules/system/network/interfaces"
