870b88d52d7153135c8264214258d835905ab49b
Vincent Bernat <vincent@bernat.im>
2014-06-08 22:11:50 +0200

    desktop: merge applications inside main desktop module
    This also avoids a nameclass with another applications class...

--- modules/desktop/manifests/init.pp
+++ modules/desktop/manifests/init.pp
@@ -11,9 +11,30 @@ class desktop {
   include fonts
 
   # Some applications
-  include applications
   include emacs
   include chromium
   include spotify
   include skype
+
+  # various stuff
+  package { "desktop-base": ensure => installed }
+  package { "evince": ensure => installed }
+  package { "geeqie": ensure => installed }
+  package { "giggle": ensure => installed }
+  package { "gimp": ensure => installed }
+  package { "gpicview": ensure => installed }
+  package { "iceweasel": ensure => installed }
+  package { "inkscape": ensure => installed }
+  package { "keepassx": ensure => installed }
+  package { "mdbus2": ensure => installed }
+  package { "mplayer": ensure => installed }
+  package { "pidgin": ensure => installed } ->
+  package { "pidgin-skype": ensure => installed }
+  package { "pinentry-gtk2": ensure => installed }
+  package { "thunar": ensure => installed }
+  package { "unison": ensure => installed }
+  package { "vim-tiny": ensure => installed }
+  package { "vlc": ensure => installed }
+  package { "wireshark": ensure => installed }
+
 }
