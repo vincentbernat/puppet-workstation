62d2ff22c4975d56ca13e9050a58c7fb0f8db495
Vincent Bernat <vincent@bernat.im>
2014-06-09 17:25:09 +0200

    x11: configure keyboard and mouse from xorg.conf
    This is quite cumbersome but we need this to support hotplug. Other
    solutions doesn't handle a keyboard that is plugged after
    configuration (or they require Gnome). The drawback is that we need to
    modify a system file and we need to restart X for changes to take
    effects...

--- modules/desktop/manifests/x11.pp
+++ modules/desktop/manifests/x11.pp
@@ -2,6 +2,33 @@ class x11 {
 
   package { "xserver-xorg": ensure => installed }
 
+  file { "/etc/X11/xorg.conf.d/10-keyboard.conf":
+    require => Package["xserver-xorg"],
+    source => "puppet:///modules/desktop/x11/keyboard.conf"
+  }
+  file { "/etc/X11/xorg.conf.d/20-mouse.conf":
+    require => Package["xserver-xorg"],
+    source => "puppet:///modules/desktop/x11/mouse.conf"
+  }
+  file { "/usr/share/X11/xkb/symbols/vbe":
+    require => Package["xserver-xorg"],
+    source => "puppet:///modules/desktop/x11/vbe"
+  }
+
+  # That's unfortunate, but we have to modify this file to get what we want...
+  define evdev_add_option($option, $symbols) {
+    file_line { "evdev_option_${option}":
+      after => "! option	=	symbols",
+      line => "  ${option} = ${symbols}",
+      path => "/usr/share/X11/xkb/rules/evdev",
+      require => Package["xserver-xorg"]
+    }
+  }
+  evdev_add_option { "vbe_pause":
+    option => "vbe:pause",
+    symbols => "+vbe(pause)"
+  }
+
   package { "lightdm": ensure => installed }
   ->
   package { "lightdm-gtk-greeter": ensure => installed }
