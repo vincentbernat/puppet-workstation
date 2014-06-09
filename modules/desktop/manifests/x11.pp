58c0d6977c75a972d01018952ad3850041b68b79
Vincent Bernat <vincent@bernat.im>
2014-06-09 18:03:17 +0200

    x11: create /etc/X11/xorg.conf.d
    xserver-xorg package does not create it.

--- modules/desktop/manifests/x11.pp
+++ modules/desktop/manifests/x11.pp
@@ -1,21 +1,24 @@
 class x11 {
 
   package { "xserver-xorg": ensure => installed }
-
+  ->
+  file { "/etc/X11/xorg.conf.d":
+    ensure => directory
+  }
   file { "/etc/X11/xorg.conf.d/10-keyboard.conf":
-    require => Package["xserver-xorg"],
+    require => File["/etc/X11/xorg.conf.d"],
     source => "puppet:///modules/desktop/x11/keyboard.conf"
   }
   file { "/etc/X11/xorg.conf.d/20-mouse.conf":
-    require => Package["xserver-xorg"],
+    require => File["/etc/X11/xorg.conf.d"],
     source => "puppet:///modules/desktop/x11/mouse.conf"
   }
   file { "/usr/share/X11/xkb/symbols/vbe":
-    require => Package["xserver-xorg"],
+    require => File["/etc/X11/xorg.conf.d"],
     source => "puppet:///modules/desktop/x11/vbe"
   }
   file { "/usr/share/X11/xkb/rules/vbe":
-    require => Package["xserver-xorg"],
+    require => File["/etc/X11/xorg.conf.d"],
     source => "/usr/share/X11/xkb/rules/evdev"
   }
 
