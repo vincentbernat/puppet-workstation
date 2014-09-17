4d18a5bf3590b34c28bf6481dc5721315bf49b51
Vincent Bernat <vincent@bernat.im>
2014-09-17 16:40:24 +0200

    fonts: use fonts-powerline package

--- modules/desktop/manifests/fonts.pp
+++ modules/desktop/manifests/fonts.pp
@@ -9,16 +9,7 @@ class fonts {
   package { "fonts-inconsolata": ensure => installed }
   package { "ttf-mscorefonts-installer": ensure => installed }
   package { "xfonts-terminus": ensure => installed }
-
-  exec { "retrieve_powerline":
-    command => "/usr/bin/wget -q https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf -O /usr/local/share/fonts/PowerlineSymbols.otf",
-    creates => "/usr/local/share/fonts/PowerlineSymbols.otf",
-    notify => Exec["build_fontconfig_cache"]
-  }
-  exec { "build_fontconfig_cache":
-    refreshonly => true,
-    command => "/usr/bin/fc-cache"
-  }
+  package { "fonts-powerline": ensure => installed }
 
   # fontconfig
   package { "fontconfig": ensure => installed }
