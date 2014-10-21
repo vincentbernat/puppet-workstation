0dbd6d156cabc851d725f7951359a701b89937ef
Vincent Bernat <vincent@bernat.im>
2014-10-21 15:31:05 +0200

    ydns: also update on DHCP renew

--- modules/system/manifests/network/ydns.pp
+++ modules/system/manifests/network/ydns.pp
@@ -8,10 +8,16 @@ class system::network::ydns($username, $password, $domain="ydns.eu") {
     owner => root,
     mode => "0700"
   }
-  ->
+
   cron { "ydns-updater":
     command => "/usr/local/bin/ydns-updater",
-    minute => '*/10'
+    minute => '*/10',
+    require => File['/usr/local/bin/ydns-updater']
+  }
+
+  file { '/etc/dhcp/dhclient-exit-hooks.d/ydns-updater':
+    ensure => link,
+    target => "/usr/local/bin/ydns-updater"
   }
 
 }
