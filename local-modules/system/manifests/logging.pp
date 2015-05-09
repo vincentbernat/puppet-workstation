e7fe7536d714ebc0b8f41b639f465ccd6f5f0ed7
Vincent Bernat <vincent@bernat.im>
2015-05-09 21:40:02 +0200

    system: set ACL on /var/log/journal

--- local-modules/system/manifests/logging.pp
+++ local-modules/system/manifests/logging.pp
@@ -12,4 +12,12 @@ class logging {
     group => "systemd-journal"
   }
 
+  exec { "Set ACL on /var/log/journal":
+    command => "/usr/bin/setfacl -R -nm g:adm:rx,d:g:adm:rx /var/log/journal",
+    unless => "/usr/bin/getfacl /var/log/journal | /bin/grep '^group:adm:r-x' && /usr/bin/getfacl /var/log/journal | /bin/grep '^default:group:adm:r-x'",
+    require => Package["acl"]
+  }
+
+  package { "acl": ensure => installed }
+
 }
