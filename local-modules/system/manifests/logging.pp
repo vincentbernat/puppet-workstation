e2a2adb76d5e75a52d2707532ef9739afb831d41
Vincent Bernat <vincent@bernat.im>
2015-05-11 11:13:12 +0200

    system: don't uninstall rsyslog
    I need it for other purposes.

--- local-modules/system/manifests/logging.pp
+++ local-modules/system/manifests/logging.pp
@@ -2,11 +2,6 @@ class logging {
 
   # Logging with systemd journal.
 
-  # No need to log with syslog anymore.
-  package { ["rsyslog", "syslog-ng"]:
-    ensure => absent
-  }
-
   file { "/var/log/journal":
     ensure => directory,
     group => "systemd-journal"
