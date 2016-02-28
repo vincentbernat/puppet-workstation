31e47d9f599873faa377b4246afc4536fb549587
Vincent Bernat <vincent@bernat.im>
2016-02-28 16:41:24 +0100

    desktop: replace fonts-droid by fonts-droid-fallback
    It's a bit unclear what "fallback" means. The package comes from another
    source and the original one was removed.

--- local-modules/desktop/manifests/fonts.pp
+++ local-modules/desktop/manifests/fonts.pp
@@ -3,7 +3,7 @@ class fonts {
   # fonts
   package { "fonts-dejavu": ensure => installed }
   package { "fonts-dejavu-extra": ensure => installed }
-  package { "fonts-droid": ensure => installed }
+  package { "fonts-droid-fallback": ensure => installed }
   package { "fonts-freefont-ttf": ensure => installed }
   package { "fonts-liberation": ensure => installed }
   package { "fonts-inconsolata": ensure => installed }
