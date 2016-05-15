69e0d3adc5bcc05d8f7a9459e6a5f0af789dc64b
Vincent Bernat <vincent@bernat.im>
2016-05-15 15:14:35 +0200

    network: add more packages

--- local-modules/tools/manifests/network.pp
+++ local-modules/tools/manifests/network.pp
@@ -2,10 +2,14 @@ class network {
 
   package { "bmon": ensure => installed }
   package { "curl": ensure => installed }
+  package { "fping": ensure => installed }
+  package { "httping": ensure => installed }
   package { "lftp": ensure => installed }
   package { "lldpd": ensure => installed }
   package { "mtr-tiny": ensure => installed }
+  package { "netcat": ensure => installed }
   package { "openssh-client": ensure => installed }
+  package { "oping": ensure => installed }
   package { "oping": ensure => installed }
   package { "snmp": ensure => installed } ->
   package { "snmp-mibs-downloader": ensure => installed }
@@ -13,5 +17,6 @@ class network {
   package { "traceroute": ensure => installed }
   package { "tshark": ensure => installed }
   package { "wget": ensure => installed }
+  package { "whois": ensure => installed }
 
 }
