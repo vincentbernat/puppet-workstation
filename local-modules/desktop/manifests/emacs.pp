a7d771dbf9900b5e693b3da0cca36232e2285272
Vincent Bernat <vincent@bernat.im>
2016-10-15 16:54:59 +0200

    emacs: update to Emacs25

--- local-modules/desktop/manifests/emacs.pp
+++ local-modules/desktop/manifests/emacs.pp
@@ -1,10 +1,10 @@
 class desktop::emacs {
 
-  package { "emacs24": ensure => installed } ->
-  package { "emacs24-el": ensure => installed }
+  package { "emacs25": ensure => installed } ->
+  package { "emacs25-el": ensure => installed }
 
-  package { "debian-el": ensure => installed, require => Package["emacs24"] }
-  package { "dpkg-dev-el": ensure => installed, require => Package["emacs24"] }
+  package { "debian-el": ensure => installed, require => Package["emacs25"] }
+  package { "dpkg-dev-el": ensure => installed, require => Package["emacs25"] }
   package { "fortune-mod": ensure => installed }
 
 }
