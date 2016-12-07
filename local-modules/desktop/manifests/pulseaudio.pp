2f2c257df79142e042fa56a0e2d4841cb82c21d0
Vincent Bernat <vincent@bernat.im>
2016-12-07 08:39:21 +0100

    pulse: disable flat volumes
    Any application, notably the browser can set the volume to 100% and
    Pulse Audio will therefore put everything to the max. Not what we want.

--- local-modules/desktop/manifests/pulseaudio.pp
+++ local-modules/desktop/manifests/pulseaudio.pp
@@ -4,7 +4,14 @@ class desktop::pulseaudio {
   package { "bluez-tools": ensure => installed }
 
   package { "pulseaudio": ensure => installed } ->
-  package { "pulseaudio-module-bluetooth": ensure => installed }
+  package { "pulseaudio-module-bluetooth": ensure => installed } ->
+  file_line { 'pulseaudio flat volumes':
+    ensure => present,
+    line   => 'flat-volumes = no',
+    match  => '^[; ]*flat-volumes = .',
+    path   => '/etc/pulse/daemon.conf'
+  }
 
   package { "pavucontrol": ensure => installed }
+
 }
