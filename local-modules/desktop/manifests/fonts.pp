71fa46f9533777814b134eb35c208d3dc4732709
Vincent Bernat <vincent@bernat.im>
2018-08-01 18:17:49 +0200

    fonts: format and alignment

--- local-modules/desktop/manifests/fonts.pp
+++ local-modules/desktop/manifests/fonts.pp
@@ -1,27 +1,27 @@
 class desktop::fonts {
 
   # fonts
-  package { "fonts-dejavu": ensure => installed }
-  package { "fonts-dejavu-extra": ensure => installed }
-  package { "fonts-droid-fallback": ensure => installed }
-  package { "fonts-freefont-ttf": ensure => installed }
-  package { "fonts-liberation": ensure => installed }
-  package { "fonts-inconsolata": ensure => purged }
-  package { "fonts-hack": ensure => installed }
-  package { "fonts-roboto": ensure => installed }
-  package { "fonts-noto-color-emoji": ensure => installed }
-  package { "ttf-mscorefonts-installer": ensure => installed }
-  package { "xfonts-terminus": ensure => installed }
-  package { "fonts-powerline": ensure => purged }
+  package { 'fonts-dejavu':              ensure => installed }
+  package { 'fonts-dejavu-extra':        ensure => installed }
+  package { 'fonts-droid-fallback':      ensure => installed }
+  package { 'fonts-freefont-ttf':        ensure => installed }
+  package { 'fonts-liberation':          ensure => installed }
+  package { 'fonts-inconsolata':         ensure => purged }
+  package { 'fonts-hack':                ensure => installed }
+  package { 'fonts-roboto':              ensure => installed }
+  package { 'fonts-noto-color-emoji':    ensure => installed }
+  package { 'ttf-mscorefonts-installer': ensure => installed }
+  package { 'xfonts-terminus':           ensure => installed }
+  package { 'fonts-powerline':           ensure => purged }
 
-  file { "/usr/local/share/fonts/PowerlineSymbols.otf":
+  file { '/usr/local/share/fonts/PowerlineSymbols.otf':
     ensure => absent
   }
 
   # fontconfig
-  package { "fontconfig": ensure => installed }
+  package { 'fontconfig': ensure => installed }
   ->
-  file { ["/etc/fonts/conf.d/10-sub-pixel-rgb.conf","/etc/fonts/conf.d/55-look-better.conf"]:
+  file { ['/etc/fonts/conf.d/10-sub-pixel-rgb.conf','/etc/fonts/conf.d/55-look-better.conf']:
     ensure => absent
   }
 
