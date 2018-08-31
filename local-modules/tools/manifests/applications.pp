629f844c640dc8f340984fb9303a3987521d804d
Vincent Bernat <vincent@bernat.ch>
2018-08-31 14:25:00 +0200

    tools: alignment

--- local-modules/tools/manifests/applications.pp
+++ local-modules/tools/manifests/applications.pp
@@ -1,20 +1,20 @@
 class tools::applications {
 
-  package { "git": ensure => installed }
-  package { "glances": ensure => installed }
-  package { "gnupg": ensure => installed }
-  package { "htop": ensure => installed }
-  package { "httpie": ensure => installed }
-  package { "imagemagick": ensure => installed }
-  package { "ioping": ensure => installed }
-  package { "iotop": ensure => installed }
-  package { "less": ensure => installed }
-  package { "lshw": ensure => installed }
-  package { "lsof": ensure => installed }
-  package { "mercurial": ensure => installed }
-  package { "silversearcher-ag": ensure => installed }
-  package { "strace": ensure => installed }
-  package { "subversion": ensure => installed }
-  package { "tmux": ensure => installed }
+  package { 'git': ensure               => installed }
+  package { 'glances': ensure           => installed }
+  package { 'gnupg': ensure             => installed }
+  package { 'htop': ensure              => installed }
+  package { 'httpie': ensure            => installed }
+  package { 'imagemagick': ensure       => installed }
+  package { 'ioping': ensure            => installed }
+  package { 'iotop': ensure             => installed }
+  package { 'less': ensure              => installed }
+  package { 'lshw': ensure              => installed }
+  package { 'lsof': ensure              => installed }
+  package { 'mercurial': ensure         => installed }
+  package { 'silversearcher-ag': ensure => installed }
+  package { 'strace': ensure            => installed }
+  package { 'subversion': ensure        => installed }
+  package { 'tmux': ensure              => installed }
 
 }
