ad55afb50e232e99562f523391b24d1acf3b1cc9
Vincent Bernat <vincent@bernat.ch>
2019-01-03 10:48:55 +0100

    system: purge mlocate

--- local-modules/system/manifests/init.pp
+++ local-modules/system/manifests/init.pp
@@ -19,7 +19,7 @@ class system {
   package { "at":              ensure => absent }
   package { "ntp":             ensure => absent }
   package { "sntp":            ensure => absent }
-  package { "mlocate":         ensure => absent }
+  package { "mlocate":         ensure => purged }
   package { "systemd-sysv":    ensure => installed } ->
   package { ["initscripts", "sysv-rc", "insserv", "startpar", "rpcbind"]:
     ensure => purged
