d674313be701bda76e3c98c16368bd04c1acbd76
Vincent Bernat <vincent@bernat.ch>
2019-01-26 17:32:31 +0100

    tools: alignment

--- local-modules/tools/manifests/network.pp
+++ local-modules/tools/manifests/network.pp
@@ -1,22 +1,22 @@
 class tools::network {
 
-  package { "bmon": ensure => installed }
-  package { "curl": ensure => installed }
-  package { "dublin-traceroute": ensure => installed }
-  package { "fping": ensure => installed }
-  package { "httping": ensure => installed }
-  package { "lftp": ensure => installed }
-  package { "lldpd": ensure => installed }
-  package { "mtr-tiny": ensure => installed }
-  package { "netcat": ensure => installed }
-  package { "openssh-client": ensure => installed }
-  package { "oping": ensure => installed }
-  package { "snmp": ensure => installed } ->
-  package { "snmp-mibs-downloader": ensure => installed }
-  package { "tcpdump": ensure => installed }
-  package { "traceroute": ensure => installed }
-  package { "tshark": ensure => installed }
-  package { "wget": ensure => installed }
-  package { "whois": ensure => installed }
+  package { 'bmon':                 ensure => installed }
+  package { 'curl':                 ensure => installed }
+  package { 'dublin-traceroute':    ensure => installed }
+  package { 'fping':                ensure => installed }
+  package { 'httping':              ensure => installed }
+  package { 'lftp':                 ensure => installed }
+  package { 'lldpd':                ensure => installed }
+  package { 'mtr-tiny':             ensure => installed }
+  package { 'netcat':               ensure => installed }
+  package { 'openssh-client':       ensure => installed }
+  package { 'oping':                ensure => installed }
+  package { 'snmp':                 ensure => installed } ->
+  package { 'snmp-mibs-downloader': ensure => installed }
+  package { 'tcpdump':              ensure => installed }
+  package { 'traceroute':           ensure => installed }
+  package { 'tshark':               ensure => installed }
+  package { 'wget':                 ensure => installed }
+  package { 'whois':                ensure => installed }
 
 }
