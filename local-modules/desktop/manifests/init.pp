5b3bdc08f35ad9fcc23e4e999ac76ff9ebaf5fa5
Vincent Bernat <vincent@bernat.ch>
2019-02-06 02:33:51 +0100

    Revert "desktop: install webext-browserpass"
    This reverts commit c9b48016c6a077a3f5553d3ce394257b244335c0. There is
    something fishy with the extension. Let's pull it from Nix instead.

--- local-modules/desktop/manifests/init.pp
+++ local-modules/desktop/manifests/init.pp
@@ -47,8 +47,7 @@ class desktop {
   }
 
   # Firefox
-  package { 'firefox':            ensure => installed } ->
-  package { 'webext-browserpass': ensure => installed }
+  package { 'firefox':            ensure => installed }
   package { 'firefox-esr':        ensure => purged }
   package { 'iceweasel':          ensure => purged }
   package { 'gstreamer1.0-vaapi': ensure => installed }
