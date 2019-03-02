60399828a58dd57a7e1d2531f8f67c4b2220467d
Vincent Bernat <vincent@bernat.ch>
2019-03-02 09:45:54 +0100

    system: don't use vsyscall anymore
    Not needed.

--- local-modules/system/manifests/init.pp
+++ local-modules/system/manifests/init.pp
@@ -82,12 +82,9 @@ class system {
     enable => true
   }
 
-  # We need vsyscall emulation to be able to use older distributions
+  # We needed vsyscall emulation to be able to use older distributions
   # (like wheezy). This is set to none with recent Debian kernels.
-  kernel_parameter { "vsyscall":
-    ensure => present,
-    value  => "emulate"
-  }
+  kernel_parameter { 'vsyscall': ensure => absent }
 
   # Enable user namespaces. This is a security risk, but I end up
   # enabling it after each boot anyway.
