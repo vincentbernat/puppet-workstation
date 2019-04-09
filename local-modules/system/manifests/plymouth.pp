018aad326d3f7c135e8289eabd7649b870ee0170
Vincent Bernat <vincent@bernat.ch>
2019-04-09 08:15:50 +0200

    system: change theme for plymouth

--- local-modules/system/manifests/plymouth.pp
+++ local-modules/system/manifests/plymouth.pp
@@ -4,15 +4,16 @@ class system::plymouth {
     ensure => installed
   }
 
+  $theme = "futureprototype"
   exec { "plymouth-set-default-theme":
-    path => [ "/sbin", "/bin", "/usr/sbin", "/usr/bin" ],
-    command => "plymouth-set-default-theme solar ; update-initramfs -u",
-    unless => "grep -qFx Theme=solar /etc/plymouth/plymouthd.conf"
+    path    => [ "/sbin", "/bin", "/usr/sbin", "/usr/bin" ],
+    command => "plymouth-set-default-theme ${theme} ; update-initramfs -u",
+    unless  => "grep -qFx Theme=${theme} /etc/plymouth/plymouthd.conf"
   }
 
   kernel_parameter { "splash":
     provider => "grub2",
-    ensure => present
+    ensure   => present
   }
 
 }
