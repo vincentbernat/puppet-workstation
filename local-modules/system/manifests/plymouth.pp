ed6b82f7e91948836497fea27b32e63560dba5a8
Vincent Bernat <vincent@bernat.ch>
2019-04-09 08:18:13 +0200

    system: fix some lints in plymouth.pp

--- local-modules/system/manifests/plymouth.pp
+++ local-modules/system/manifests/plymouth.pp
@@ -1,19 +1,19 @@
 class system::plymouth {
 
-  package { ["plymouth", "plymouth-themes"]:
+  package { ['plymouth', 'plymouth-themes']:
     ensure => installed
   }
 
-  $theme = "futureprototype"
-  exec { "plymouth-set-default-theme":
-    path    => [ "/sbin", "/bin", "/usr/sbin", "/usr/bin" ],
+  $theme = 'futureprototype'
+  exec { 'plymouth-set-default-theme':
+    path    => [ '/sbin', '/bin', '/usr/sbin', '/usr/bin' ],
     command => "plymouth-set-default-theme ${theme} ; update-initramfs -u",
     unless  => "grep -qFx Theme=${theme} /etc/plymouth/plymouthd.conf"
   }
 
-  kernel_parameter { "splash":
-    provider => "grub2",
-    ensure   => present
+  kernel_parameter { 'splash':
+    ensure   => present,
+    provider => 'grub2'
   }
 
 }
