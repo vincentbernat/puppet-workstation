41e84f01c0875630f5e17a46b0061aaeeeaf158c
Vincent Bernat <vincent@bernat.ch>
2019-07-07 12:21:30 +0200

    x11: ensure libvdpau-va-gl1 is not installed
    It may trick some software to think we have a VDPAU enabled graphic
    card. This can be good or bad (notably, it can prevent using native
    VAAPI).

--- local-modules/desktop/manifests/x11.pp
+++ local-modules/desktop/manifests/x11.pp
@@ -1,23 +1,24 @@
 class desktop::x11 {
 
-  package { ["xserver-xorg",
-             "xserver-xorg-input-evdev",
-             "xserver-xorg-input-libinput",
-             "va-driver-all"]: ensure => installed }
-  package { ["xserver-xorg-input-synaptics",
-             "xserver-xorg-legacy"]: ensure => absent }
+  package { ['xserver-xorg',
+             'xserver-xorg-input-evdev',
+             'xserver-xorg-input-libinput',
+             'va-driver-all']: ensure => installed }
+  package { ['xserver-xorg-input-synaptics',
+             'xserver-xorg-legacy',
+             'libvdpau-va-gl1']: ensure => absent }
 
-  package { "lightdm": ensure => installed }
+  package { 'lightdm': ensure => installed }
   ->
-  package { "lightdm-gtk-greeter": ensure => installed }
+  package { 'lightdm-gtk-greeter': ensure => installed }
   ->
-  file { "/etc/lightdm/lightdm.conf":
-    content => template("desktop/x11/lightdm.conf.erb")
+  file { '/etc/lightdm/lightdm.conf':
+    content => template('desktop/x11/lightdm.conf.erb')
   }
   ~>
-  service { "lightdm":
+  service { 'lightdm':
     ensure => running,
-    restart => "service lightdm reload"
+    restart => 'service lightdm reload'
   }
 
 }
