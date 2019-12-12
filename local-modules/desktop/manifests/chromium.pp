687e29eeaabf367d7ad54d9f0e75548803d7d172
Vincent Bernat <vincent@bernat.ch>
2019-12-12 18:39:41 +0100

    chromium: don't manage the default-flags file
    It seems we are now in sync with Debian.

--- local-modules/desktop/manifests/chromium.pp
+++ local-modules/desktop/manifests/chromium.pp
@@ -2,16 +2,4 @@ class desktop::chromium {
   package { ["chromium", "chromium-l10n"]: ensure => installed }
   ->
   package { ["google-chrome-beta", "google-talkplugin"]: ensure => purged }
-  ->
-  file { '/etc/chromium.d':
-    ensure  => directory,
-    recurse => true,
-    purge   => true,
-    source => "puppet:///modules/desktop/chromium"
-  }
-  ->
-  file { '/etc/chromium.d/apikeys':
-    # Provided with Debian package, leave as is
-    ensure => present
-  }
 }
