3086394ae293f397a83296d1c0bc4e2ff4f1b9ba
Vincent Bernat <vincent@bernat.ch>
2020-01-04 18:42:49 +0100

    desktop: don't install libreoffice-java-common

--- local-modules/desktop/manifests/init.pp
+++ local-modules/desktop/manifests/init.pp
@@ -39,6 +39,7 @@ class desktop {
   package { 'libreoffice':               ensure => installed }
   package { 'libreoffice-gtk3':          ensure => installed }
   package { 'libreoffice-sdbc-firebird': ensure => purged }
+  package { 'libreoffice-java-common':   ensure => purged }
   package { ['gvfs-daemons', 'gvfs-backends']:
     ensure => purged
   }
