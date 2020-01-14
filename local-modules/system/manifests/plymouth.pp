734f096b095e192e6d57b0f68c6adafd279d1ec2
Vincent Bernat <vincent@bernat.ch>
2020-01-14 08:57:42 +0100

    plymouth: simplify Grub update by invoking it after unpacking theme

--- local-modules/system/manifests/plymouth.pp
+++ local-modules/system/manifests/plymouth.pp
@@ -9,7 +9,7 @@ class system::plymouth {
     creates => '/usr/share/plymouth/themes/fallout/fallout.plymouth',
     command => @(END)
       curl -Ls https://dllb2.pling.com/api/files/download/id/1536334015/s/c5839b97d39daf8d7f57d3e323827ca9fde18dca954c84e32cf67e1e0d485c1274a91c2c929d5d36e62e0800e8a204d7e91f37ae69b17a4989dbca98e1b0df48/t/1578992005/c/3b20c163f90850c9c92e3e332dee592b931fda1f81c492953300aa31e6d2d038e497140d3fc4cfa75ba4960689756127638697c3d5fb677ca6d806ef55337ea5/lt/download/fallout.tar.xz \
-        | tar -C /usr/share/plymouth/themes -zxvf -
+        | tar -C /usr/share/plymouth/themes -Jxvf -
       | END
   } ->
   exec { 'plymouth-set-default-theme':
@@ -23,20 +23,16 @@ class system::plymouth {
   file { ['/boot/grub/themes', '/boot/grub/themes/fallout']:
     ensure => directory
   } ->
+  file { '/etc/default/grub':
+    source => 'puppet:///modules/system/grub'
+  } ->
   exec { 'unpack grub fallout theme':
     path    => [ '/sbin', '/bin', '/usr/sbin', '/usr/bin' ],
     creates => '/boot/grub/themes/fallout/theme.txt',
     command => @(END)
       curl -Ls https://github.com/shvchk/fallout-grub-theme/archive/master.tar.gz \
         | tar -C /boot/grub/themes/fallout --strip-components=1 -zxvf -
+      update-grub
       | END
-  } ->
-  file { '/etc/default/grub':
-    source => 'puppet:///modules/system/grub'
-  } ~>
-  exec { 'update-grub':
-    command     => '/sbin/update-grub',
-    refreshonly => true
   }
-
 }
