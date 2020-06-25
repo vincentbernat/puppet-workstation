ee2576fdb904320dbe4127f922ff883ecbcfeab0
Vincent Bernat <vincent@bernat.ch>
2020-06-25 07:27:14 +0200

    cowbuilder: install hooks for lintian andautopkgtest

--- local-modules/dev/manifests/cowbuilder.pp
+++ local-modules/dev/manifests/cowbuilder.pp
@@ -26,5 +26,20 @@ class dev::cowbuilder {
     mode    => '0755',
     source  => "puppet:///modules/dev/pbuilder/hooks"
   }
+  file { "/etc/pbuilder/hooks/B90lintian":
+    ensure  => link,
+    target  => "/usr/share/doc/pbuilder/examples/B90lintian",
+    require => Package["cowbuilder"],
+  }
+  file { "/etc/pbuilder/hooks/B20autopkgtest":
+    ensure  => link,
+    target  => "/usr/share/doc/pbuilder/examples/B20autopkgtest",
+    require => Package["cowbuilder"],
+  }
+  file { "/etc/pbuilder/hooks/D80no-man-db-rebuild":
+    ensure  => link,
+    target  => "/usr/share/doc/pbuilder/examples/D80no-man-db-rebuild",
+    require => Package["cowbuilder"],
+  }
 
 }
