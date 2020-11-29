64a11c266b1db98c0c6b7c88b2eab8f8dbeffa50
Vincent Bernat <vincent@bernat.ch>
2020-11-29 08:44:35 +0100

    tools: reformat list

--- local-modules/tools/manifests/applications.pp
+++ local-modules/tools/manifests/applications.pp
@@ -1,33 +1,37 @@
 class tools::applications {
 
-  package { "jq": ensure                  => installed }
-  package { "moreutils": ensure           => installed }
-  package { 'bpftrace': ensure            => installed }
-  package { 'bat': ensure                 => installed }
-  package { 'bpytop': ensure              => installed }
-  package { 'colordiff': ensure           => installed }
-  package { 'glances': ensure             => installed }
-  package { 'gnupg': ensure               => installed }
-  package { 'htop': ensure                => installed }
-  package { 'httpie': ensure              => installed }
-  package { 'imagemagick': ensure         => installed }
-  package { 'ioping': ensure              => installed }
-  package { 'iotop': ensure               => installed }
-  package { 'less': ensure                => installed }
-  package { 'linux-perf': ensure          => installed }
-  package { 'lshw': ensure                => installed }
-  package { 'lsof': ensure                => installed }
-  package { 'mercurial': ensure           => installed }
-  package { 'neofetch':ensure             => installed }
-  package { 'pass': ensure                => installed }
-  package { 'pass-extension-otp': ensure  => installed }
-  package { 'pass-extension-tail': ensure => installed }
-  package { 'silversearcher-ag': ensure   => installed }
-  package { 'strace': ensure              => installed }
-  package { 'subversion': ensure          => installed }
-  package { 'tmux': ensure                => installed }
-  package { 'tzdiff': ensure              => installed }
-  package { 'urlscan': ensure             => installed }
+  package { [
+             'bat',
+             'bpftrace',
+             'bpytop',
+             'colordiff',
+             'glances',
+             'gnupg',
+             'htop',
+             'httpie',
+             'imagemagick',
+             'ioping',
+             'iotop',
+             'jq',
+             'less',
+             'linux-perf',
+             'lshw',
+             'lsof',
+             'mercurial',
+             'moreutils',
+             'neofetch',
+             'pass',
+             'pass-extension-otp',
+             'pass-extension-tail',
+             'silversearcher-ag',
+             'strace',
+             'subversion',
+             'tmux',
+             'tzdiff',
+             'urlscan',
+             ]:
+               ensure => installed
+  }
   package { "linux-headers-${::os['architecture']}":
     ensure => installed }
 
