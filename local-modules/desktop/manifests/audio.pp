7ba33d98f3a8d8ea1d2a03e51804216ed27a7ff5
Vincent Bernat <vincent@bernat.ch>
2021-03-20 08:29:36 +0100

    audio: revert to PulseAudio
    On X1 Carbon, all devices seem unavailable for some reason. I'll need
    some time to investigate...

--- local-modules/desktop/manifests/audio.pp
+++ local-modules/desktop/manifests/audio.pp
@@ -1,31 +1,20 @@
 class desktop::audio {
-  # Configure Pipewire with bluetooth support
+  # Configure pulseaudio with bluetooth support
   package { ["bluez",
              "bluez-tools",
-             "pavucontrol"]:
+             "pavucontrol",
+             "pulseaudio",
+             "pulseaudio-module-bluetooth"]:
                ensure => installed
   }
   package { ["pipewire",
              "pipewire-audio-client-libraries"]:
-               ensure => installed,
-               install_options => ["-t", "experimental"]
-  } ->
-  file { ["/etc/pipewire/media-session.d/with-pulseaudio",
-          "/etc/pipewire/media-session.d/with-alsa",
-          "/etc/pipewire/media-session.d/with-jack"]:
-            ensure => present,
-            content => ""
-  } ->
+               ensure => purged
+  }
   file { "/etc/alsa/conf.d/99-pipewire-default.conf":
-    ensure => link,
-    target => "/usr/share/doc/pipewire/examples/alsa.conf.d/99-pipewire-default.conf"
-  } ->
+    ensure => absent,
+  }
   file { "/etc/ld.so.conf.d/pipewire-jack.conf":
-    ensure => link,
-    target => "/usr/share/doc/pipewire/examples/ld.so.conf.d/pipewire-jack-${facts['platform']}.conf"
-  } ~>
-  exec { "refresh ldconfig cache":
-    command => '/usr/sbin/ldconfig',
-    refreshonly => true
+    ensure => absent
   }
 }
