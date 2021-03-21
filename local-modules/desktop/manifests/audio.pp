0fa4eccabc50ccd822cf4a652913cabf191aae0a
Vincent Bernat <vincent@bernat.ch>
2021-03-21 10:01:41 +0100

    audio: cleanup after removing PipeWire

--- local-modules/desktop/manifests/audio.pp
+++ local-modules/desktop/manifests/audio.pp
@@ -7,14 +7,4 @@ class desktop::audio {
              "pulseaudio-module-bluetooth"]:
                ensure => installed
   }
-  package { ["pipewire",
-             "pipewire-audio-client-libraries"]:
-               ensure => purged
-  }
-  file { "/etc/alsa/conf.d/99-pipewire-default.conf":
-    ensure => absent,
-  }
-  file { "/etc/ld.so.conf.d/pipewire-jack.conf":
-    ensure => absent
-  }
 }
