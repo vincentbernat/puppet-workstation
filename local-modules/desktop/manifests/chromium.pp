d532861af52ac7f4daa50e3399725cccfde67df6
Vincent Bernat <vincent@bernat.ch>
2021-03-29 16:00:40 +0200

    chromium: reinstall Google Chrome

--- local-modules/desktop/manifests/chromium.pp
+++ local-modules/desktop/manifests/chromium.pp
@@ -1,5 +1,17 @@
 class desktop::chromium {
+  # Chromium
   package { ["chromium", "chromium-l10n"]: ensure => installed }
+
+  # Google Chrome
+  apt::source { 'google-chrome':
+    location     => 'http://dl.google.com/linux/chrome/deb/',
+    release      => 'stable',
+    repos        => 'main',
+    key          => '9534C9C4130B4DC9927992BF4F30B6B4C07CB649',
+    include      => { 'src' => false },
+    pin          => 400,
+    architecture => 'amd64'
+  }
   ->
-  package { ["google-chrome-beta", "google-talkplugin"]: ensure => purged }
+  package { 'google-chrome-stable': ensure => installed }
 }
