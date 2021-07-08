fd10b476716f83036b8770df94524316978e939c
Vincent Bernat <vincent@bernat.ch>
2021-07-08 22:01:46 +0200

    boot: use a theme easier to download
    And it looks cool.

--- local-modules/system/manifests/boot.pp
+++ local-modules/system/manifests/boot.pp
@@ -1,22 +1,22 @@
 class system::boot {
 
-  # Plymouth theme: https://www.gnome-look.org/p/1259515/
+  # Plymouth theme:
   package { ['plymouth', 'plymouth-themes']:
     ensure => installed
   } ->
-  exec { 'unpack plymouth fallout theme':
+  exec { 'unpack plymouth theme':
     require => Package['curl'],
     path    => [ '/sbin', '/bin', '/usr/sbin', '/usr/bin' ],
-    creates => '/usr/share/plymouth/themes/fallout/fallout.plymouth',
+    creates => '/usr/share/plymouth/themes/glitch/glitch.plymouth',
     command => @(END)
-      curl -Ls https://dllb2.pling.com/api/files/download/id/1536334015/s/c5839b97d39daf8d7f57d3e323827ca9fde18dca954c84e32cf67e1e0d485c1274a91c2c929d5d36e62e0800e8a204d7e91f37ae69b17a4989dbca98e1b0df48/t/1578992005/c/3b20c163f90850c9c92e3e332dee592b931fda1f81c492953300aa31e6d2d038e497140d3fc4cfa75ba4960689756127638697c3d5fb677ca6d806ef55337ea5/lt/download/fallout.tar.xz \
-        | tar -C /usr/share/plymouth/themes -Jxvf -
+      curl -Ls https://github.com/adi1090x/files/raw/master/plymouth-themes/themes/pack_2/glitch.tar.gz \
+        | tar -C /usr/share/plymouth/themes -zxvf -
       | END
   } ->
   exec { 'plymouth-set-default-theme':
     path    => [ '/sbin', '/bin', '/usr/sbin', '/usr/bin' ],
-    command => "plymouth-set-default-theme fallout ; update-initramfs -u",
-    unless  => "grep -qFx Theme=fallout /etc/plymouth/plymouthd.conf"
+    command => "plymouth-set-default-theme glitch ; update-initramfs -u",
+    unless  => "grep -qFx Theme=glitch /etc/plymouth/plymouthd.conf"
   }
 
   # Grub theme
