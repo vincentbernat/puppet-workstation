cc6a437cec9b92a3cb9f043e6f56089a43417b2b
Vincent Bernat <vincent@bernat.ch>
2021-07-08 23:52:20 +0200

    docker: add user to docker group

--- local-modules/system/manifests/docker.pp
+++ local-modules/system/manifests/docker.pp
@@ -1,6 +1,10 @@
 class system::docker {
   package { "docker.io":         ensure => installed }
   ->
+  User <| title == "${::user}" |> {
+    groups +> "docker"
+  }
+  ->
   file { "/etc/docker/daemon.json":
     content => @(END)
       {"default-address-pools": [{"base":"10.207.0.0/16","size":24}]}
