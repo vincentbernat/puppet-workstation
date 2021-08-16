5dc8a296c4861a89a5e1fdcd6f52a9fd52b66db5
Vincent Bernat <vincent@bernat.ch>
2021-08-16 16:15:57 +0200

    ddns: make it optional

--- local-modules/system/manifests/network/ddns.pp
+++ local-modules/system/manifests/network/ddns.pp
@@ -1,29 +1,30 @@
 #
 # Dynamic DNS update using Route53
 #
-class system::network::ddns($key, $secret, $domain, $ttl=60) {
+class system::network::ddns($key=undef, $secret=undef, $domain=undef, $ttl=60) {
 
-  file { "/usr/local/bin/ddns-updater":
-    content => template("system/network/ddns-updater.erb"),
-    owner => root,
-    mode => "0700"
+  if $domain != undef {
+    file { "/usr/local/bin/ddns-updater":
+      content => template("system/network/ddns-updater.erb"),
+      owner => root,
+      mode => "0700"
+    }
+
+    $mstart = fqdn_rand(15)
+    $sleep = fqdn_rand(40)
+    cron { "ddns-updater":
+      command => "/bin/sleep ${sleep} ; [ x`/usr/bin/nmcli networking connectivity` != xfull ] || /usr/local/bin/ddns-updater --ipv6=public",
+      minute => "${mstart}-59/15",
+      require => File['/usr/local/bin/ddns-updater']
+    }
+
+    file { '/etc/dhcp/dhclient-exit-hooks.d/ddns-updater':
+      source => "puppet:///modules/system/network/dhcp-hook"
+    }
+
+    ensure_resource(
+      package,
+      'python3-boto',
+      { ensure => present })
   }
-
-  $mstart = fqdn_rand(15)
-  $sleep = fqdn_rand(40)
-  cron { "ddns-updater":
-    command => "/bin/sleep ${sleep} ; [ x`/usr/bin/nmcli networking connectivity` != xfull ] || /usr/local/bin/ddns-updater --ipv6=public",
-    minute => "${mstart}-59/15",
-    require => File['/usr/local/bin/ddns-updater']
-  }
-
-  file { '/etc/dhcp/dhclient-exit-hooks.d/ddns-updater':
-    source => "puppet:///modules/system/network/dhcp-hook"
-  }
-
-  ensure_resource(
-    package,
-    'python3-boto',
-    { ensure => present })
-
 }
