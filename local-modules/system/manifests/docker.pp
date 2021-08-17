e9ee3b3340c448476567ed2611fca0f0ed644d10
Vincent Bernat <vincent@bernat.ch>
2021-08-17 15:22:43 +0200

    docker: try to outsmart Docker

--- local-modules/system/manifests/docker.pp
+++ local-modules/system/manifests/docker.pp
@@ -7,7 +7,7 @@ class system::docker {
   ->
   file { "/etc/docker/daemon.json":
     content => @(END)
-      {"default-address-pools": [{"base":"10.207.0.0/16","size":24}]}
+      {"default-address-pools": [{"base":"100.207.0.0/16","size":24}]}
       | END
   }
   ~>
