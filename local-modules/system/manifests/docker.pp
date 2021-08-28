295d7675f5ed3fb0abe54c573a39cc17fa2cd3a6
Vincent Bernat <vincent@bernat.ch>
2021-08-28 13:59:10 +0200

    docker: fix range used for Docker to be in 100.64/10

--- local-modules/system/manifests/docker.pp
+++ local-modules/system/manifests/docker.pp
@@ -8,7 +8,7 @@ class system::docker {
   ->
   file { "/etc/docker/daemon.json":
     content => @(END)
-      {"default-address-pools": [{"base":"100.207.0.0/16","size":24}]}
+      {"default-address-pools": [{"base":"100.107.0.0/16","size":24}]}
       | END
   }
   ~>
