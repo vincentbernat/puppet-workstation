85e60ef5008049ee6a45ba6c4d02d7d48e698b83
Vincent Bernat <vincent@bernat.ch>
2021-09-01 07:13:48 +0200

    desktop: remove google chrome as well

--- local-modules/desktop/manifests/chromium.pp
+++ local-modules/desktop/manifests/chromium.pp
@@ -1,7 +1,4 @@
 class desktop::chromium {
-  # Chromium
-  package { ["chromium", "chromium-l10n"]: ensure => purged }
-
-  # Google Chrome
-  package { 'google-chrome-stable': ensure => installed }
+  # Chromium/Google Chrome (we use the Flatpak)
+  package { ["chromium", "chromium-l10n", "google-chrome-stable"]: ensure => purged }
 }
