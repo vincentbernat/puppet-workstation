b67d2bce1e3e1a3c4fa1af932e6cd3968c848b1e
Vincent Bernat <vincent@bernat.ch>
2021-12-06 22:47:57 +0100

    emacs: explain a bit the various parts
    Notably, the rationale of the two ELPA package I install as Debian packages.

--- local-modules/desktop/manifests/emacs.pp
+++ local-modules/desktop/manifests/emacs.pp
@@ -2,14 +2,20 @@ class desktop::emacs {
 
   package { 'emacs':    ensure => installed } ->
   package { 'emacs-el': ensure => installed } ->
+
+  # I do not like what many Debian packages do on start. That's not
+  # needed. Most of the time, autoloading works just fine.
   file { "/etc/emacs/site-start.d":
     ensure  => directory,
     purge   => true,
     recurse => true
   }
 
+  # The ones on MELPA stable are not totally up-to-date
   package { 'elpa-debian-el':   ensure => installed, require => Package['emacs'] }
   package { 'elpa-dpkg-dev-el': ensure => installed, require => Package['emacs'] }
+
+  # Various related tools
   package { 'fortune-mod':      ensure => installed }
   package { 'gpgsm':            ensure => installed }
   package { 'zbar-tools':       ensure => installed }
