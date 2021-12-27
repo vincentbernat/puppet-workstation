a3659949900133f14ea3508fb920a1c6c7558f38
Vincent Bernat <vincent@bernat.ch>
2021-12-27 11:55:28 +0100

    audio: switch back to PulseAudio

--- local-modules/desktop/manifests/audio.pp
+++ local-modules/desktop/manifests/audio.pp
@@ -1,19 +1,12 @@
 class desktop::audio {
-  # Configure pipewire with bluetooth support
+  # Configure pulseaudio with bluetooth support
   package { ["bluez",
              "bluez-tools",
              "pavucontrol",
              "gstreamer1.0-fdkaac",
-             "pipewire",
-             "pipewire-pulse",
-             "pipewire-audio-client-libraries",
-             "wireplumber",
-             "libspa-0.2-bluetooth"]:
+             "pulseaudio",
+             "pulseaudio-module-bluetooth"]:
                ensure => installed
   }
-  package { ["pipewire-media-session",
-             "pulseaudio-module-bluetooth",
-             "pulseaudio"]:
-               ensure => purged
-  }
+  package { "pipewire": ensure => purged }
 }
