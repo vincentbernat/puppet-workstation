65fb9994f59d2907a3798c2b73533f947008d2b2
Vincent Bernat <vincent@bernat.ch>
2022-01-04 09:47:13 +0100

    dev: install poetry

--- local-modules/dev/manifests/init.pp
+++ local-modules/dev/manifests/init.pp
@@ -32,6 +32,7 @@ class dev {
   package { 'python3-virtualenv': ensure => installed }
   package { 'python3-pip':        ensure => installed }
   package { 'python3-jedi':       ensure => installed }
+  package { 'python3-poetry':     ensure => installed }
   package { 'black':              ensure => installed }
   package { 'pyflakes3':          ensure => installed }
   package { 'mypy':               ensure => installed }
