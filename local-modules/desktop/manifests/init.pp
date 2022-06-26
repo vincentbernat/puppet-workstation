197cf17956d34b5a2f725d9e990ffa5beed45f2d
Vincent Bernat <vincent@bernat.ch>
2022-06-26 13:53:51 +0200

    Revert "desktop: replace Spotify by Deezer with MellowPlayer"
    This reverts commit c963fc8b2c6da3089200c33968112bf2877e07c7.

--- local-modules/desktop/manifests/init.pp
+++ local-modules/desktop/manifests/init.pp
@@ -118,7 +118,9 @@ class desktop {
     remote => "flathub-beta"
   }
   desktop::flatpak { "us.zoom.Zoom": }
-  desktop::flatpak { "com.gitlab.ColinDuquesnoy.MellowPlayer": }
+  desktop::flatpak { "com.spotify.Client":
+    permissions => "filesystems=!xdg-pictures"
+  }
   desktop::flatpak { "com.discordapp.Discord":
     permissions => "filesystems=!xdg-pictures;!xdg-videos"
   }
