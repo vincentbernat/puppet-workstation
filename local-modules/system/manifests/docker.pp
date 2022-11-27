2be1079c03f06ea06e2fddfa9727257df3fd1bd6
Vincent Bernat <vincent@bernat.ch>
2022-11-27 11:46:56 +0100

    system: don't put main user into docker group
    One can use podman instead.

--- local-modules/system/manifests/docker.pp
+++ local-modules/system/manifests/docker.pp
@@ -3,10 +3,6 @@ class system::docker {
              "docker-compose",
              "podman"]: ensure => installed }
   ->
-  User <| title == "${::user}" |> {
-    groups +> "docker"
-  }
-  ->
   file { "/etc/docker/daemon.json":
     content => @(END)
       {"default-address-pools": [{"base":"100.107.0.0/16","size":24}],
