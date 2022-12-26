36ab91955e2912ab52510fd98d69bdeca7d9cf3b
Vincent Bernat <vincent@bernat.ch>
2022-12-26 10:07:14 +0100

    audio: enable bluez D-Bus interface

--- local-modules/desktop/manifests/audio.pp
+++ local-modules/desktop/manifests/audio.pp
@@ -9,7 +9,13 @@ class desktop::audio {
   } -> service { "rtkit-daemon":
     ensure => running,
     enable => true
-  }
+  } -> file_line { "enable Bluetooth D-Bus interface":
+    ensure => present,
+    line   => 'Experimental = true',
+    match  => '^#?Experimental = .*',
+    path   => '/etc/bluetooth/main.conf',
+  } ~> service { "bluetooth": }
+
   package { ["gstreamer1.0-fdkaac",
              "pulseaudio",
              "pulseaudio-module-bluetooth"]: ensure => purged }
