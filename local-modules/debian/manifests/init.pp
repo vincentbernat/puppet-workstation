ae2ce93baaa8168c87d6e94e5f3967615f120874
Vincent Bernat <vincent@bernat.ch>
2023-05-31 14:47:38 +0200

    debian: switch to fr mirror

--- local-modules/debian/manifests/init.pp
+++ local-modules/debian/manifests/init.pp
@@ -67,13 +67,13 @@ class debian {
     include  => { 'src' => false }
   }
   apt::source { 'testing':
-    location          => 'http://ftp.de.debian.org/debian/',
+    location          => 'http://ftp.fr.debian.org/debian/',
     release           => 'testing',
     repos             => 'main',
   }
 
   apt::source { 'experimental':
-    location => 'http://ftp.de.debian.org/debian/',
+    location => 'http://ftp.fr.debian.org/debian/',
     release  => 'experimental',
     repos    => 'main',
     include  => { 'src' => true }
