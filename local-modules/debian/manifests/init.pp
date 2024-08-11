558946078990013feba1c63a91b3f0f02448d78b
Vincent Bernat <vincent@bernat.ch>
2024-08-11 10:28:02 +0200

    debian: pin mesa to experimental for now

--- local-modules/debian/manifests/init.pp
+++ local-modules/debian/manifests/init.pp
@@ -89,12 +89,17 @@ class debian {
   apt::pin { ['experimental', 'experimental-debug']:
     priority => 101
   }
-  # apt::pin { 'prefer-experimental':
-  #   priority => 990,
-  #   packages => [
-  #   ],
-  #   release => "experimental"
-  # }
+  apt::pin { 'prefer-experimental':
+    priority => 990,
+    packages => [
+      # See https://gitlab.freedesktop.org/mesa/mesa/-/commit/bceb2328c48a3eb8a9039911393fb2a7c23d5ad1
+      # And https://gitlab.freedesktop.org/drm/amd/-/issues/3437
+      # And https://gitlab.freedesktop.org/drm/amd/-/issues/3497
+      "mesa-*",
+      "*-mesa*"
+    ],
+    release => "experimental"
+  }
 
   package { 'flatpak': ensure => installed }
   package { 'needrestart': ensure => installed }
